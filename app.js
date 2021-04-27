8f6e29e09150f76548812f615154d8fb445caeaf
posix <posix.research@gmail.com>
2021-04-27 08:34:01 +0000

    -

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 const fs = require('fs');
 const readline = require('readline');
 const process = require('process');
+const sqlite3 = require('sqlite3');
+const { scan, delay } = require('./lib/util');
 
-const { scan } = require('./lib/util');
-const domainList = process.argv[2] ? process.argv[2] : './resource/domain.csv';
+const db = new sqlite3.Database('./resource/db.sql');
 
-(async function() {
-    const fileStream = fs.createReadStream(domainList);
-
-    const chunk = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-
-    for await (const url of chunk) {
-        if (url == '') continue;
-
-        try {
-            let target;
-
-            if (url.startsWith('http://') || url.startsWith('https://')) {
-                target = new URL(url);
-            } else {
-                target = new URL('http://' + url);
+(async function loop() {
+    await db.get('select * from domains where flag = 0 limit 1', async (err, res) => {
+        if (!res) {
+            if (err) console.log(err);
+            console.log(new Date);
+            await delay(3000);
+        } else {
+            const { url } = res;
+    
+            /* prevent dup */
+            db.run('update domains set flag = 1 where url = ?', url);
+    
+            try {
+                let target;
+    
+                if (url.startsWith('http://') || url.startsWith('https://')) {
+                    target = new URL(url);
+                } else {
+                    target = new URL('http://' + url);
+                }
+    
+                await scan(target);
+            } catch (e) {
+                console.log(url, e);
             }
-
-            await scan(target);
-        } catch (e) {
-            console.log(url, e);
         }
-    }
+
+        loop();
+    });
 })();
